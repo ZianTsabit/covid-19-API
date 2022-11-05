42a2fcedba630f0368886807cd1974f0cc57320b
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-11-05 16:14:23 +0700

    make endpoint

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-print('hello world')
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/api/bq_v1/answer_sql1")
+def answer_sql1():
+    return {"Hello": "World"}
+
+@app.get("/api/bq_v1/answer_sql2")
+def answer_sql2():
+    return {"Hello": "World"}
