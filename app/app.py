96afa8df9c2d795c0eb85a86e24cec799e135951
ghaziankamil <16520465@std.stei.itb.ac.id>
2022-11-06 05:58:06 +0700

    finish work

--- app/app.py
+++ app/app.py
@@ -1,21 +1,52 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from google.cloud import bigquery
+from codecs import open
+import os
 
-
-
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "app\credentials.json"
 app = FastAPI()
 client = bigquery.Client()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World !"}
 
 @app.get("/api/bq_v1/answer_sql1")
 def answer_sql1():
-    return {"Hello": "World"}
+
+    sqlfile = "sql/answer_sql1.sql"
+    query = open(sqlfile, mode='r', encoding='utf-8-sig').read() 
+    query_job = client.query(query)
+    rows = query_job.result()   
+    data = None
+    for row in rows:
+        data = row[0]
+    
+    if data is None:
+        return {"message": "No data found"}
+    else:
+        return {
+            "message": "Successfully retrieved data total persons cumulatively vaccinated in Indonesia on June 1, 2022 ",
+            "data": data
+        }
 
 @app.get("/api/bq_v1/answer_sql2")
 def answer_sql2():
-    return {"Hello": "World"}
+    
+    sqlfile = "sql/answer_sql2.sql"
+    query = open(sqlfile, mode='r', encoding='utf-8-sig').read() 
+    query_job = client.query(query)
+    rows = query_job.result()   
+    data = None
+    for row in rows:
+        data = row[0]
+
+    if data is None:
+        return {"message": "No data found"}
+    else:
+        return {
+            "message": "Successfully retrieved data total confirmed case in Indonesia during January 1, 2022",
+            "data": data
+        }
